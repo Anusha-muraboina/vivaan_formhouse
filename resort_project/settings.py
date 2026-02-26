fc0530c07b131f853abc122f346132888f7c9664
anushamuraboina99 <anushamuraboina99@gmail.com>
2026-02-26 18:43:34 +0530

    updated the setting
    updated the setting

--- resort_project/settings.py
+++ resort_project/settings.py
@@ -13,8 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-strawberry-king-resort-secret-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     "vivaanfarmhouse.com",
     "www.vivaanfarmhouse.com",
